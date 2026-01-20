8c40eab574976ec4ec5b70689a528db35381e16b
samuelmota321 <samuel.mota.2010@gmail.com>
2026-01-20 16:31:31 -0300

    create username

--- src/app/dashboard/me/_actions/createUserName.tsx
+++ src/app/dashboard/me/_actions/createUserName.tsx
@@ -1,5 +1,8 @@
 'use server'
 import { z } from 'zod'
+import { auth } from '@/lib/auth'
+import { prisma } from '@/lib/prisma'
+import { error } from 'console'
 
 const createUserNameSchema = z.object({
   username: z.string().min(4, "É necessário que seu username tenha pelo menos 4 caracteres")
@@ -8,16 +11,46 @@ const createUserNameSchema = z.object({
 type createUserNameFormData = z.infer<typeof createUserNameSchema>
 
 export async function createUserName(data: createUserNameFormData) {
-  const schema = createUserNameSchema.safeParse(data)
-  console.log("função chamada2")
 
-  if(!schema.success) {
+  const session = await auth()
+
+  if (!session?.user) {
     return {
-      data: null, 
-      error: schema.error
+      data: null,
+      error: "Usuário não autenticado"
     }
   }
 
-  return data.username
+  const schema = createUserNameSchema.safeParse(data)
+
+  if (!schema.success) {
+    return {
+      data: null,
+      error: schema.error.issues[0].message
+    }
+  }
+
+  try {
+    const userId = session.user.id
+    await prisma.user.update({
+      where: {
+        id: userId as string
+      },
+      data: {
+        username: data.username
+      }
+    })
+
+    return  {
+      data: "Username criado com sucesso",
+      error: null
+    }
+
+  } catch (err) {
+    return {
+      data: null,
+      error: "Falha ao atualizar o username"
+    }
+  }
 
 }
--- src/app/dashboard/me/_components/url.tsx
+++ src/app/dashboard/me/_components/url.tsx
@@ -1,8 +1,11 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import { createUserName } from '../_actions/createUserName'
+import { useState } from 'react'
 
 export const UrelPreview = () => {
+
+  const [error, setError] = useState<null | string>(null)
 
   const submitAction = async (formData: FormData) => {
     const username = formData.get('username') as string
@@ -10,36 +13,42 @@ export const UrelPreview = () => {
     if (username === "") {
       return
     }
-    
+
     const response = await createUserName({ username })
-    console.log(response)
+    if (response.error) {
+      setError(response.error)
+    }
   }
 
   return (
-    <div className="flex items-center flex-1 p-2 text-gray-100">
-      <form
-        className="flex flex-1 flex-col md:flex-row gap-4 items-start md:items-center"
-        action={submitAction}
-      >
-        <div className="flex items-center justify-center w-full">
-          <p>
-            {process.env.NEXT_PUBLIC_HOST_URL}/creator/
-          </p>
-          <input
-            type="text"
-            className="flex-1 outline-none border h-9 border-gray-300 text-black rounded-md bg-gray-50"
-            placeholder='Digite seu username'
-            name='username'
-          />
-        </div>
-
-        <Button
-          type='submit'
-          className='bg-blue-500 hover:bg-blue-600 h-9 w-full md:w-fit text-white px-4 rounded-md  cursor-pointer'
+    <div className='w-full'>
+      <div className="flex items-center flex-1 p-2 text-gray-100">
+        <form
+          className="flex flex-1 w-full flex-col md:flex-row gap-2 items-start md:items-center"
+          action={submitAction}
         >
-          Salvar
-        </Button>
-      </form>
+          <div className="flex flex-col md:flex-row items-center justify-center w-full">
+            <p>
+              {process.env.NEXT_PUBLIC_HOST_URL}/creator/
+            </p>
+            <input
+              type="text"
+              className="flex flex-1 outline-none border h-9 border-gray-300 text-black rounded-md bg-gray-50 p-2"
+              placeholder='Digite seu username'
+              name='username'
+            />
+          </div>
+
+          <Button
+            type='submit'
+            className='bg-blue-500 hover:bg-blue-600 h-9 w-full md:w-fit text-white px-4 rounded-md  cursor-pointer'
+          >
+            Salvar
+          </Button>
+        </form>
+      </div>
+          {error && <p className='text-red-500'>{error}</p>}
     </div>
+
   )
 }
